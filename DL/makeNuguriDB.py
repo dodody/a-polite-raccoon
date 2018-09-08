98183afe0c20ccd2861a4c6b0515e03ce549dcc0
Sung Man Cho <pccho9108@gmail.com>
2018-09-08 16:24:29 +0900

    makeNuguriDB.py added

---
+++ DL/makeNuguriDB.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+import sys
+
+if len(sys.argv)==2 :
+    csv_file_name = sys.argv[1]
+
+    ### 1. read csv
+    # Please Download Google Spread Sheet 
+    # intput Google Spread Sheet Name 
+    data = pd.read_csv(csv_file_name)
+
+    # data divide
+    input_data = data.iloc[:,0]
+    output_data = data.iloc[:,1]
+
+    # write .txt file until input data size 
+    f = open("data/user-nuguri.txt",'w')
+    for i in range(len(input_data)):
+        indata = input_data[i] + '\t' + output_data[i] + '\n'
+        f.write(indata)
--- DL/seq2seq_translation_tutorial.py
+++ DL/seq2seq_translation_tutorial.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*-
+#*- coding: utf-8 -*-
 """
 Translation with a Sequence to Sequence Network and Attention
 *************************************************************
